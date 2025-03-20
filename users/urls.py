f4aec5739fec4780d4868a4c587338ae03a2d971
syrok32 <stepstepan2@gmail.com>
2025-03-20 15:15:06 +0300

    add crud

--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,16 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import PaymentListAPIView, UserCreateAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView, UserDeleteAPIView, UserUpdateAPIView, UserRetrieveAPIView,UserListAPIView
 
 app_name = 'users'
 urlpatterns = [
     path('pay/', PaymentListAPIView.as_view(), name='pay'),
     path('login/', TokenObtainPairView.as_view(permission_classes = [AllowAny]), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', UserCreateAPIView.as_view(), name='register')
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('delete/<int:pk>', UserDeleteAPIView.as_view(), name='delete'),
+    path('list/', UserListAPIView.as_view(), name='list'),
+    path('update/<int:pk>', UserUpdateAPIView.as_view(), name='update'),
+    path('retriew/<int:pk>      ', UserRetrieveAPIView.as_view(), name='retr')
 ]
