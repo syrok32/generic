0d2d404f9d9312cfadf6aee81cbbc129e9bc4a43
syrok32 <stepstepan2@gmail.com>
2025-04-12 23:24:56 +0300

    ci cd 5

--- users/management/commands/create_moderator_group.py
+++ users/management/commands/create_moderator_group.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
-from users.models import User  # Замените на вашу модель пользователя
 
 
 class Command(BaseCommand):
